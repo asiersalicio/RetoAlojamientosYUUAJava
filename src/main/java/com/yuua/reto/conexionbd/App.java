8e9c8640d8fa6e6153820b5b467ee47f36a0c31c
asiersalicio <46441325+asiersalicio@users.noreply.github.com>
2019-12-16 12:37:54 +0100

    Add files via upload

---
+++ src/main/java/com/yuua/reto/conexionbd/App.java
@@ -0,0 +1,13 @@
+package com.yuua.reto.conexionbd;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
