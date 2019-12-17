e1c3c2443f97345d57c29045da168a7325d3f764
Guasy12 <unai__13@hotmail.com>
2019-12-17 14:09:27 +0100

    Reserva entidad

--- src/main/java/com/yuua/reto/entidades/Reserva.java
+++ src/main/java/com/yuua/reto/entidades/Reserva.java
@@ -4,6 +4,8 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToOne;
@@ -14,16 +16,71 @@ import javax.persistence.Table;
 public class Reserva {
 
 	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
 	@Column(name = "idReserva") 
 	private int id;
 	@Column(name = "fechaEntrada")
 	private Date fechaEntrada;
 	@Column(name = "fechaSalida")
 	private Date fechaSalida;
-	
+	@OneToOne
+    @JoinColumn(name = "idAlojamiento")
+    private Alojamientos alojamiento;
 	@OneToOne
     @JoinColumn(name = "idDni")
     private Usuario usuario;
-}
+	
+	protected Reserva() {
+		
+	}
 
-//RESERVA (#ID_RESERVA(PK), FECHA_ENTRADA, FECHA_SALIDA, ID_ALOJAMIENTO(FK), ID_DNI(FK))
+	public Reserva(int id, Date fechaEntrada, Date fechaSalida, Alojamientos alojamiento, Usuario usuario) {
+		super();
+		this.id = id;
+		this.fechaEntrada = fechaEntrada;
+		this.fechaSalida = fechaSalida;
+		this.alojamiento = alojamiento;
+		this.usuario = usuario;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Date getFechaEntrada() {
+		return fechaEntrada;
+	}
+
+	public void setFechaEntrada(Date fechaEntrada) {
+		this.fechaEntrada = fechaEntrada;
+	}
+
+	public Date getFechaSalida() {
+		return fechaSalida;
+	}
+
+	public void setFechaSalida(Date fechaSalida) {
+		this.fechaSalida = fechaSalida;
+	}
+
+	public Alojamientos getAlojamiento() {
+		return alojamiento;
+	}
+
+	public void setAlojamiento(Alojamientos alojamiento) {
+		this.alojamiento = alojamiento;
+	}
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+	
+}
