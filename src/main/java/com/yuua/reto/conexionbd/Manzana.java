a77fbf2d0cc0005936f65f9a8165553f8d645da0
YaroslavDemyanenko <46441353+YaroslavDemyanenko@users.noreply.github.com>
2019-12-17 13:05:32 +0100

    clase manzana

--- src/main/java/com/yuua/reto/conexionbd/Manzana.java
+++ src/main/java/com/yuua/reto/conexionbd/Manzana.java
@@ -2,53 +2,54 @@ package com.yuua.reto.conexionbd;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 
-
 @Entity
 @Table(name = "manzanas")
-
 public class Manzana {
 
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	//@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Id
-	@Column (name = "id")
-	int id;	
-	@Column (name = "peso")
+	@Column(name = "id")
+	int id;
+	@Column(name = "peso")
 	double peso;
-	@Column (name = "especie")
+	@Column(name = "especie")
 	String especie;
-	
-	
+
 	protected Manzana() {
 	}
+
 	public Manzana(int id, double peso, String especie) {
 		super();
 		this.id = id;
 		this.peso = peso;
 		this.especie = especie;
 	}
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public double getPeso() {
 		return peso;
 	}
+
 	public void setPeso(double peso) {
 		this.peso = peso;
 	}
+
 	public String getEspecie() {
 		return especie;
 	}
+
 	public void setEspecie(String especie) {
 		this.especie = especie;
 	}
-	
-	
+
 }
