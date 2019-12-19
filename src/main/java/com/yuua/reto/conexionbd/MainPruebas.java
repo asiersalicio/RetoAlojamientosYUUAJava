8cfbb47b0f13480730ad2b750d8894083cfc5062
YaroslavDemyanenko <46441353+YaroslavDemyanenko@users.noreply.github.com>
2019-12-19 13:36:27 +0100

    Update MainPruebas.java

--- src/main/java/com/yuua/reto/conexionbd/MainPruebas.java
+++ src/main/java/com/yuua/reto/conexionbd/MainPruebas.java
@@ -7,7 +7,7 @@ import com.yuua.reto.net.Server;
 public class MainPruebas {
 
 	public static void main(String[] args) {
-		Alojamiento aloj = new Alojamiento(5, "CAMPING", "Camping el calvo de la loteria", "Hay que tener fe", 94400000, "http://www.calvoloteria.com/", "calvo@loterias.es", 2);
+		Alojamiento aloj = new Alojamiento(5, "CAMPING", "Camping el calvo de la loteria", "Hay que tener fe", 94400000, "http://www.calvoloteria.com/", "calvo@loterias.es", 2, null);
 		System.out.println(JsonParser.parseAlojamiento(aloj));
 
 		Server servidor = new Server();
