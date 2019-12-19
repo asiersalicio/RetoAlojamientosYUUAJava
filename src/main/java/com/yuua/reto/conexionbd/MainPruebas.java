41c71041d7c577acec70d265f60b78901ff6324b
asiersalicio <salicioasier99@gmail.com>
2019-12-19 12:42:38 +0100

    Sockets

--- src/main/java/com/yuua/reto/conexionbd/MainPruebas.java
+++ src/main/java/com/yuua/reto/conexionbd/MainPruebas.java
@@ -2,6 +2,7 @@ package com.yuua.reto.conexionbd;
 
 import com.yuua.reto.entidades.Alojamiento;
 import com.yuua.reto.json.JsonParser;
+import com.yuua.reto.net.Server;
 
 public class MainPruebas {
 
@@ -9,6 +10,8 @@ public class MainPruebas {
 		Alojamiento aloj = new Alojamiento(5, "CAMPING", "Camping el calvo de la loteria", "Hay que tener fe", 94400000, "http://www.calvoloteria.com/", "calvo@loterias.es", 2);
 		System.out.println(JsonParser.parseAlojamiento(aloj));
 
+		Server servidor = new Server();
+		
 	}
 
 }
