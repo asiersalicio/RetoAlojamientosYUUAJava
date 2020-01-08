7572c40dfed316e047d257f2c10c610e529261db
YaroslavDemyanenko <46441353+YaroslavDemyanenko@users.noreply.github.com>
2020-01-08 08:21:44 +0100

    Servidor creado

--- src/main/java/com/yuua/reto/conexionbd/MainPruebas.java
+++ src/main/java/com/yuua/reto/conexionbd/MainPruebas.java
@@ -1,10 +1,15 @@
 package com.yuua.reto.conexionbd;
 
+import com.yuua.reto.net.Server;
+
 public class MainPruebas {
 
 	public static void main(String[] args) {
-		
-		
+
+		Server servidor = new Server();
+		Thread hiloserver = new Thread(servidor);
+		hiloserver.start();
+
 	}
 
 }
--- src/main/java/com/yuua/reto/net/Server.java
+++ src/main/java/com/yuua/reto/net/Server.java
@@ -1,61 +1,96 @@
 package com.yuua.reto.net;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.ArrayList;
 
-public class Server extends Thread{
+public class Server implements Runnable {
 
-	public static int puerto = 55555;
-	private ServerSocket servidor;
-	private ArrayList<Socket> clientes;
-	private DataOutputStream salida;
-	private DataInputStream entrada;
-	
-	public Server() 
-	{
-		servidor = null;
-		try {
-			servidor = new ServerSocket(puerto);
-			clientes = new ArrayList<Socket>();
-			start();
-			
-			
-		} catch (IOException e) {
-			System.out.println("Error: " + e.getMessage());
-		}
-	}
-	
-	public void esperarAccionesUsuario()
-	{
-		try {
-			entrada.readUTF();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+	private final int PUERTO = 5000;
+	public ServerSocket servidor = null;
+	public Socket cliente = null;
+	public ObjectInputStream entrada = null;
+	public ObjectOutputStream salida = null;
+
+	public Server() {
 	}
 
 	@Override
 	public void run() {
-		
-		System.out.println("Esperando conexiones del cliente...");
+		servidor = null;
+		cliente = null;
+		entrada = null;
+		salida = null;
 		try {
-			clientes.add(servidor.accept());
-			System.out.println("Cliente conectado");
-			salida = new DataOutputStream(clientes.get(0).getOutputStream());
-			entrada = new DataInputStream(clientes.get(0).getInputStream());
-			salida.writeUTF("ENVIADO DESDE EL SERVIDOR");
-			System.out.println("Enviado correctamente");
+			servidor = new ServerSocket(PUERTO);
+
+			while (true) {
+				System.out.println("Esperando conexiones del cliente...");
+				cliente = servidor.accept();
+				System.out.println("Conexion realizada");
+				salida = new ObjectOutputStream(cliente.getOutputStream());
+				entrada = new ObjectInputStream(cliente.getInputStream());
+				Request peticion = (Request) entrada.readObject();
+				switch (peticion.getCodigoPeticion()) {
+				case 0:
+					System.out.println(peticion.getObjetoEnviado());
+					break;
+
+				default:
+					break;
+				}
+				cliente.close();
+			}
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+			System.out.println("Error: " + e.getMessage());
+		} catch (Exception e) {
+			System.out.println("Error: " + e.getMessage());
+		} finally {
+			try {
+				if (servidor != null)
+					servidor.close();
+				if (cliente != null)
+					cliente.close();
+				if (entrada != null)
+
+					entrada.close();
+				if (salida != null)
+					salida.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			System.out.println("Fin servidor");
 		}
-		
 	}
-	
+
+	/**
+	 * public static int puerto = 55555; private ServerSocket servidor; private
+	 * ArrayList<Socket> clientes; private DataOutputStream salida; private
+	 * DataInputStream entrada;
+	 * 
+	 * public Server() { servidor = null; try { servidor = new ServerSocket(puerto);
+	 * clientes = new ArrayList<Socket>();
+	 * 
+	 * 
+	 * } catch (IOException e) { System.out.println("Error: " + e.getMessage()); } }
+	 * 
+	 * public void esperarAccionesUsuario() { try { entrada.readUTF(); } catch
+	 * (IOException e) { // TODO Auto-generated catch block e.printStackTrace(); } }
+	 * 
+	 * @Override public void run() {
+	 * 
+	 *           System.out.println("Esperando conexiones del cliente..."); try {
+	 *           clientes.add(servidor.accept()); System.out.println("Cliente
+	 *           conectado"); salida = new
+	 *           DataOutputStream(clientes.get(0).getOutputStream()); entrada = new
+	 *           DataInputStream(clientes.get(0).getInputStream());
+	 *           salida.writeUTF("ENVIADO DESDE EL SERVIDOR");
+	 *           System.out.println("Enviado correctamente"); } catch (IOException
+	 *           e) { // TODO Auto-generated catch block e.printStackTrace(); }
+	 * 
+	 *           }
+	 **/
 
 }
